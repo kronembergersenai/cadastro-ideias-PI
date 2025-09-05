e91f4f50cfc3995467046186cf7284449cd6518c
kronembergersenai <31590363+kronembergersenai@users.noreply.github.com>
2025-09-04 21:23:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.getElementById('ideaForm');
 const msg  = document.getElementById('msg');
 
 // Substitua pela URL do seu Web App do Apps Script
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbytiJXW31NfYQ5Ub31jltVnNqxr6gSBbQRPcKjsjDllFk3btArXqtkJrZ0bDAazNa8X/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz8Nl8UB0uQvZjuZK--VVuxgH_CTMZoHR6bUzrL9wu3lCDYZ2bSeqD9ubjXRC-uuWR4/exec';
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
